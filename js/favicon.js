dd462ecc499ba5bccd20098dc30c0f315b4beb0e
Verpissesdich <72820713+Verpissesdich@users.noreply.github.com>
2024-10-19 21:37:59 +0400

    first commit

---
+++ js/favicon.js
@@ -0,0 +1,20 @@
+﻿
+// скрипт на JS для работы favicona из 26ти катинок
+let counter = 1;
+let faviconImgs = 26;
+let favicons = document.querySelectorAll("link");
+
+function animateFavicon() {
+    favicons.forEach(item =>{
+       if(item.getAttribute("rel").indexOf("icon") >= 0){
+           item.setAttribute("href", "images/favicon/" + counter + ".png");
+       }
+    });
+    counter++;
+    if(counter == faviconImgs){ counter = 1; }
+
+}
+
+setInterval(animateFavicon, 100);
+
+
